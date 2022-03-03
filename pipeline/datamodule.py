794600c9cb4ef2fbcda81b51fc43b8639dc9519d
aleksandr.snorkin <aleksandr.snorkin@g42.ai>
2022-03-03 15:30:28 +0400

    Color augmentations

--- pipeline/datamodule.py
+++ pipeline/datamodule.py
@@ -17,7 +17,8 @@ class HappyLightningDataModule(pl.LightningDataModule):
         self.augmentations = [
             A.HorizontalFlip(),
             A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.15, rotate_limit=30, border_mode=0, value=0, p=0.5),
-            A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.1, p=0.5),
+            A.HueSaturationValue(hue_shift_limit=3, sat_shift_limit=70, val_shift_limit=0, p=0.5),
+            A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.2, p=0.5),
         ]
         self.post_transforms = [
             A.Resize(height=self.hparams.input_height, width=self.hparams.input_width, always_apply=True),

e95eb2d220f6a8aae1bfa27ffc9352f41b11ce07
aleksandr.snorkin <aleksandr.snorkin@g42.ai>
2022-03-09 14:43:37 +0400

    Augmentations color oneof

--- pipeline/datamodule.py
+++ pipeline/datamodule.py
@@ -18,6 +18,11 @@ class HappyLightningDataModule(pl.LightningDataModule):
         self.augmentations = [
             HorizontalFlip(num_classes=self.hparams.num_classes, flip_id=self.hparams.flip_id, p=0.5),
             A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.15, rotate_limit=30, border_mode=0, value=0, p=0.5),
+            A.OneOf([
+                A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
+                A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.2, p=0.5),
+                A.ToGray(p=0.05),
+            ], p=0.75),
             A.HueSaturationValue(hue_shift_limit=3, sat_shift_limit=70, val_shift_limit=0, p=0.5),
             A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.2, p=0.5),
         ]

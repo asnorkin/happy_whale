bd1853d56f5a02d813f47ba36be4e858c1015f0b
aleksandr.snorkin <aleksandr.snorkin@g42.ai>
2022-03-09 15:17:15 +0400

    image_utils added

---
+++ utils/image_utils.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+
+
+def get_3rd_point(a, b):
+    """Return vector c that perpendicular to (a - b)."""
+    direct = a - b
+    return b + np.array([-direct[1], direct[0]], dtype=np.float32)
+
+
+def get_dir(src_point, rot_rad):
+    """Rotate the point by `rot_rad` degree."""
+    sn, cs = np.sin(rot_rad), np.cos(rot_rad)
+
+    src_result = [0, 0]
+    src_result[0] = src_point[0] * cs - src_point[1] * sn
+    src_result[1] = src_point[0] * sn + src_point[1] * cs
+
+    return src_result
+
+
+def _center_scale_to_box(center, scale):
+    pixel_std = 1.0
+    w = scale[0] * pixel_std
+    h = scale[1] * pixel_std
+    xmin = center[0] - w * 0.5
+    ymin = center[1] - h * 0.5
+    xmax = xmin + w
+    ymax = ymin + h
+    bbox = [xmin, ymin, xmax, ymax]
+
+    return bbox
+
+
+def _box_to_center_scale(x, y, w, h, aspect_ratio=1.0, scale_mult=1.25):
+    """Convert box coordinates to center and scale.
+    adapted from https://github.com/Microsoft/human-pose-estimation.pytorch
+    """
+    pixel_std = 1
+    center = np.zeros((2), dtype=np.float32)
+    center[0] = x + w * 0.5
+    center[1] = y + h * 0.5
+
+    if w > aspect_ratio * h:
+        h = w / aspect_ratio
+    elif w < aspect_ratio * h:
+        w = h * aspect_ratio
+    scale = np.array(
+        [w * 1.0 / pixel_std, h * 1.0 / pixel_std], dtype=np.float32)
+    if center[0] != -1:
+        scale = scale * scale_mult
+
+    return center, scale
+
+
+def get_affine_transform(center,
+                         scale,
+                         rot,
+                         output_size,
+                         shift=np.array([0, 0], dtype=np.float32),
+                         inv=0):
+    if not isinstance(scale, np.ndarray) and not isinstance(scale, list):
+        scale = np.array([scale, scale])
+
+    scale_tmp = scale
+    src_w = scale_tmp[0]
+    dst_w = output_size[0]
+    dst_h = output_size[1]
+
+    rot_rad = np.pi * rot / 180
+    src_dir = get_dir([0, src_w * -0.5], rot_rad)
+    dst_dir = np.array([0, dst_w * -0.5], np.float32)
+
+    src = np.zeros((3, 2), dtype=np.float32)
+    dst = np.zeros((3, 2), dtype=np.float32)
+    src[0, :] = center + scale_tmp * shift
+    src[1, :] = center + src_dir + scale_tmp * shift
+    dst[0, :] = [dst_w * 0.5, dst_h * 0.5]
+    dst[1, :] = np.array([dst_w * 0.5, dst_h * 0.5]) + dst_dir
+
+    src[2:, :] = get_3rd_point(src[0, :], src[1, :])
+    dst[2:, :] = get_3rd_point(dst[0, :], dst[1, :])
+
+    if inv:
+        trans = cv2.getAffineTransform(np.float32(dst), np.float32(src))
+    else:
+        trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+    return trans
+
+
+def crop_center_scale(img_cv, bbox, input_size, scale_mult=1.0):
+    """
+    Crops, centers, expands and resizes bounding box keeping aspect ratio while
+    centering over a black canvas removing background outside bounding box
+    """
+    aspect_ratio = float(input_size[1]) / input_size[0]
+
+    x0, y0, x1, y1 = bbox
+
+    x0, y0, x1, y1 = round(float(x0)), round(float(y0)), round(float(x1)), round(float(y1))
+    img_masked = np.zeros_like(img_cv)
+    img_masked[y0:y1, x0:x1] = img_cv[y0:y1, x0:x1]
+
+    center, scale = _box_to_center_scale(x0, y0, x1 - x0, y1 - y0, aspect_ratio, scale_mult)
+    scale = scale * 1.0
+
+    inp_h, inp_w = input_size
+
+    trans = get_affine_transform(center, scale, 0, [inp_w, inp_h])
+    img_crop = cv2.warpAffine(img_masked, trans, (int(inp_w), int(inp_h)), flags=cv2.INTER_LINEAR)
+    bbox = _center_scale_to_box(center, scale)
+
+    return img_crop, bbox

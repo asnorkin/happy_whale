ed2eb0cbfe79be6f20420eda12b0f60ffcbd6440
aleksandr.snorkin <aleksandr.snorkin@g42.ai>
2022-03-26 16:52:40 +0400

    fit_fuse update

--- fit_fuse.sh
+++ fit_fuse.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,
+export CUDA_VISIBLE_DEVICES=0,
 export OPENBLAS_NUM_THREADS=1
 
 source ../.whale_venv/bin/activate
@@ -17,13 +17,15 @@ WORKERS=16
 FOLD=0
 LR=0.001
 M=0.4
+DROP_FIN=0.5
 
-EXPERIMENT=${GPUS}g${BATCH}x${ACC}b${EPOCHS}e${LR}lr${FOLD}f
+EXPERIMENT=df${DROP_FIN}_${GPUS}g${BATCH}x${ACC}b${EPOCHS}e${LR}lr${FOLD}f
 
 export PYTHONPATH=.:${PYTHONPATH}
 
 nohup python ${WORK_DIR}/train.py \
     --precision=16 \
+    --drop_fin_prob=${DROP_FIN} \
     --m=${M} \
     --labels_csv=${LABELS_CSV} \
     --image_ids_file=${IMAGE_IDS_FILE} \
